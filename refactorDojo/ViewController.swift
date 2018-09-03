18360b7943eb2f3ae89dbb3dcda22a971f7b0299
Brad Phillips <brad@bradphillips.org>
2018-09-03 17:16:31 -0500

    Initial Commit

---
+++ refactorDojo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  refactorDojo
+//
+//  Created by Brad Phillips on 9/3/18.
+//  Copyright © 2018 megaBreezy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
