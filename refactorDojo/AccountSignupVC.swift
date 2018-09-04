4d20faa007300906493df8202117d746436b988a
Keithon Stribling <K.Strib@Keithons-MacBook-Pro.local>
2018-09-04 17:13:49 -0500

    Added new class for new button that dismisses signup screen.

---
+++ refactorDojo/AccountSignupVC.swift
@@ -0,0 +1,37 @@
+//
+//  AccountSignupVC.swift
+//  refactorDojo
+//
+//  Created by Keithon Stribling on 9/4/18.
+//  Copyright © 2018 megaBreezy. All rights reserved.
+//
+
+import UIKit
+
+class AccountSignupVC: UIViewController {
+	
+	/** Properties **/
+	
+	@IBOutlet weak var backButton: UIButton!
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Do any additional setup after loading the view.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	/** Action **/
+	
+	@IBAction func backButtonPress(_ sender: UIButton) {
+		self.dismiss(animated: true, completion: nil)
+	}
+	
+	
+	
+}
